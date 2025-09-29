b6015e24701af7da351cbc468e8d274d0b779150
RodrigoQVF <rodrigoqvf10@gmail.com>
2025-09-29 19:18:23 -0300

    feat: Adicionado a linha de código para definir /AdmPannel como Endpoint para o painel do administrador

--- src/Pages/Router/App.jsx
+++ src/Pages/Router/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import Home from '../Home/Home.jsx';
 import Cadastro from '../Cadastro/Cadastro.jsx'; 
 import Login from '../Login/Login.jsx';
+import PainelAdm from '../painelAdm/painelAdm.jsx';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/cadastro" element={<Cadastro />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/AdmPannel" element={<PainelAdm />} />
     </Routes>
   );
 }
