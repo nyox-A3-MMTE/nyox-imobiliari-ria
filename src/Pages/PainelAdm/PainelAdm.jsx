425aafe25651c5dc421a0158e933b4a5829b8dc3
RodrigoQVF <rodrigoqvf10@gmail.com>
2025-09-29 19:15:00 -0300

    feat: Criação da pagina aonde ficará localizada o painel do administrador para fazer a administração dos imoveis cadastrados

---
+++ src/Pages/PainelAdm/PainelAdm.jsx
@@ -0,0 +1,14 @@
+import './painelAdm.module.css';
+import Sidebar from '../../Components/Sidebar/Sidebar';
+
+function PainelAdm() {
+  return (
+    <>
+    <Sidebar/>
+
+
+    </>
+  )
+}
+
+export default PainelAdm
