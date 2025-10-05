717e614b11d2817b1195501839073296b8fd1a4e
RodrigoQVF <rodrigoqvf10@gmail.com>
2025-10-04 22:02:39 -0300

    feat: Adição dos botões de editar e excluir o imovel cadastrado

--- src/Pages/PainelAdm/PainelAdm.jsx
+++ src/Pages/PainelAdm/PainelAdm.jsx
@@ -22,12 +22,37 @@ function PainelAdm() {
         }
       });
       if (response.ok) {
+
+        console.log("Lista de imoveis carregada com sucesso!");
+        
         const data = await response.json();
         setImoveis(data); 
       } else {
         console.error('Erro na resposta do servidor');
       }
     } catch (error) {
+      console.error('Erro ao conectar ao servidor:', error);
+    }
+  }
+
+
+
+  async function handleDelete(id) {
+    try {
+      const response = await fetch(`http://localhost:8800/imoveis/delete/${id}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        }
+      });
+            if (response.ok) {
+            const deletedImovel = await response.json();
+            setImoveis(prevImoveis => prevImoveis.filter(imovel => imovel.id !== deletedImovel.id));
+
+      } else {
+        console.error('Erro na resposta do servidor');
+        }
+      } catch (error) {
       console.error('Erro ao conectar ao servidor:', error);
     }
   }
@@ -46,16 +71,25 @@ function PainelAdm() {
         <h1>Imóveis disponíveis!</h1>
         {imoveis.map((imovel, index) => (
           <div key={index} className="imovel">
-            <h2>{imovel.descricao}</h2>
-            <p>Bairro: {imovel.bairro}</p>
-            <p>Cidade: {imovel.cidade}</p>
-            <p>Estado: {imovel.estado}</p>
-            <p>Tipo: {imovel.tipo}</p>
-            <p>Quartos: {imovel.quartos}</p>
-            <p>Banheiros: {imovel.banheiros}</p>
-            <p>Vagas na garagem: {imovel.vagas_garagem}</p>
-            <p>Área total: {imovel.area_total} m²</p>
-            <p>Valor: {imovel.valor}</p>
+            <div>
+              <p>{imovel.id}</p>
+              <h2>{imovel.descricao}</h2>
+              <p>Bairro: {imovel.bairro}</p>
+              <p>Cidade: {imovel.cidade}</p>
+              <p>Estado: {imovel.estado}</p>
+              <p>Tipo: {imovel.tipo}</p>
+              <p>Quartos: {imovel.quartos}</p>
+              <p>Banheiros: {imovel.banheiros}</p>
+              <p>Vagas na garagem: {imovel.vagas_garagem}</p>
+              <p>Área total: {imovel.area_total} m²</p>
+              <p>Valor: {imovel.valor}</p>
+            </div>
+            <div className='containerBotoes'>
+              <div className='botoes'>
+                <button className='editar'>Editar</button>
+                <button className='excluir' onClick={() => handleDelete(imovel.id)}>Excluir</button>
+              </div>
+            </div>
           </div>
         ))}
     </div>
